17f8a9b0745a1b6ac90fe06057687efba5b16bb5
KING_ <89888075+kkssbbb@users.noreply.github.com>
2024-03-26 16:35:04 +0900

    refactor: QueryDslConfig 설정
    내용:
    JPAQueryFactory 에 EntityManager 의존성 주입후 @Bean 등록

---
+++ src/main/java/com/team/RecipeRadar/global/config/querydsl/QueryDslConfig.java
@@ -0,0 +1,17 @@
+package com.team.RecipeRadar.global.config.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class QueryDslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
